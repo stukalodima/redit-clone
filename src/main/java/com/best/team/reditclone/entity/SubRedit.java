201cac1ce0b230895f8b5d061cea8d4de75be6c5
Ihor Rozhkov <ihorrozhkov@gmail.com>
2024-03-02 17:19:09 +0200

    fix SubRedit & User classes

--- src/main/java/com/best/team/reditclone/entity/SubRedit.java
+++ src/main/java/com/best/team/reditclone/entity/SubRedit.java
@@ -1,36 +1,19 @@
 package com.best.team.reditclone.entity;
 
-import java.util.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-/**
- * 
- */
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubRedit {
 
-    /**
-     * Default constructor
-     */
-    public SubRedit() {
-    }
-
-    /**
-     * 
-     */
     private Long id;
-
-    /**
-     * 
-     */
     private String name;
-
-    /**
-     * 
-     */
     private LocalDateTime createdDate;
-
-    /**
-     * 
-     */
     private User user;
 
 }
--- src/main/java/com/best/team/reditclone/entity/User.java
+++ src/main/java/com/best/team/reditclone/entity/User.java
@@ -1,61 +1,21 @@
 package com.best.team.reditclone.entity;
 
-import java.util.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-/**
- * 
- */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-
-    /**
-     * Default constructor
-     */
-    public User() {
-    }
-
-    /**
-     * 
-     */
     private Long id;
-
-    /**
-     * 
-     */
     private String displayName;
-
-    /**
-     * 
-     */
     private String login;
-
-    /**
-     * 
-     */
     private String firstName;
-
-    /**
-     * 
-     */
     private String lastName;
-
-    /**
-     * 
-     */
     private String password;
-
-    /**
-     * 
-     */
     private byte ava;
-
-    /**
-     * 
-     */
     private String email;
-
-    /**
-     * 
-     */
     private boolean active;
 
 }
